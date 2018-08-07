dcf50beacf719fadbd8dceb8d3cd2f8973ab910d
pravallikadamacherla <41770543+pravallikadamacherla@users.noreply.github.com>
2018-08-07 13:48:26 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
-d=['a','e','i','o','u','A','E','I','O','U'];
-s=str(input("Enter the letter:"))
-if(s in d):
+p=['a','e','i','o','u','A','E','I','O','U'];
+s=str(input())
+if(s in p):
   print("vowel")
 else:
   print("consonant")
